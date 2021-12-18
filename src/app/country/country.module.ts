a24055c1d4185fe16c97a7b5b04d057cf3179d1d
sorayaleon <sorayaleongon@gmail.com>
2021-12-18 20:42:11 +0100

    Add byCapital, byCountry, byRegion and showCountry components

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
+import { ByCountryComponent } from './pages/by-country/by-country.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { ShowCountryComponent } from './pages/show-country/show-country.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalComponent,
+    ByCountryComponent,
+    ByRegionComponent,
+    ShowCountryComponent
+  ],
   imports: [
     CommonModule
   ]
