1dc27dac056aac0e4f96dbdff3b418fe1ade2506
sorayaleon <soraya.l.g.88@gmail.com>
2022-02-15 16:19:55 +0100

    Conditional CSS classes

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -9,9 +9,16 @@ import { Component } from '@angular/core';
 export class ByRegionComponent {
 
   regions: string[] = ['africa', 'america', 'asia', 'europe', 'oceania'];
+  
+  // regions: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
+
   activeRegion: string = '';
 
   constructor() { }
+
+  getCSSClass(region: string) : string {
+    return (region === this.activeRegion) ? 'btn btn-primary mx-1' : 'btn btn-outline-primary mx-1'
+  }
 
   activateRegion( region: string) {
     this.activeRegion = region;
